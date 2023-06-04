6db907faa5a04ee71bd493bf4959f3b39cca4d42
Костенко Вадим <kostenko@mdrk.ru>
2023-06-04 18:42:04 +0700

    cors

--- backend/app.js
+++ backend/app.js
@@ -23,15 +23,20 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://cozystyle.nomoredomains.monster/');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
   next();
-});
+  });
+
 app.use(requestLogger);
 app.use(router);
 app.use(errorLogger);
