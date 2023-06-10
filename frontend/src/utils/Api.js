e06c9418c3295d11de628eccda0181eec07d654a
Костенко Вадим <kostenko@mdrk.ru>
2023-06-10 14:02:19 +0700

    cors

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -93,6 +93,7 @@ const apiConfig = {
   headers: {
     'Content-Type': 'application/json',
     'Origin': 'http://127.0.0.1:3000',
+    'Authorization': `Bearer ${localStorage.getItem('token')}`
   }
 }
 const api = new Api(apiConfig); 
