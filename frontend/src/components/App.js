8aa05755d0a7517812cce9847fd2a8a2be17d649
Костенко Вадим <kostenko@mdrk.ru>
2023-06-11 13:58:28 +0700

    cors

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -88,7 +88,7 @@ function App() {
   function handleCardLike(card) {
     // Снова проверяем, есть ли уже лайк на этой карточке
     const isLiked = card.likes.some(i => i._id === currentUser._id);    
-    console.log(currentUser);
+    console.log(currentUser, card, isLiked);
     // Отправляем запрос в API и получаем обновлённые данные карточки
     api.changeLikeCardStatus(card._id, isLiked).then((newCard) => {
       setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
