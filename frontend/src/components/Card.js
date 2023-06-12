a07b7bd49bbbda7d12e34aa28b81f59edd141d62
Костенко Вадим <kostenko@mdrk.ru>
2023-06-12 22:53:02 +0700

    15pr

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -4,10 +4,8 @@ import { CurrentUserContext } from "../contexts/CurrentUserContext";
 function Card (props) {
 
   const currentUser = React.useContext(CurrentUserContext);
-
   // Определяем, являемся ли мы владельцем текущей карточки
   const isOwn = props.card.owner === currentUser._id;
-
   // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
   const isLiked = props.card.likes.some((i) => i === currentUser._id);
   const cardLikeButtonClassName = ( 
